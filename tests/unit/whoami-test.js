e5aa5dbee58b01d4fd4b3ba2e59b16fcb5254a68
Chris Manson <chris@stonecircle.io>
2018-05-17 16:03:12 +0100

    adding test to verify which dummy app is running

---
+++ tests/unit/whoami-test.js
@@ -0,0 +1,8 @@
+import { module, test } from 'qunit'
+import config from '../../config/environment'
+
+module(config.modulePrefix + '-tests')
+
+test('it works!', function(assert) {
+  assert.ok(true);
+})
